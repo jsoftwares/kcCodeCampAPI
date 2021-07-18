90f24d1a8e842e85db8a3e815211021825cee962
Jeffrey Onochie <jeff.ict@gmail.com>
2021-07-18 07:28:09 +0100

    add database seeder & fix typo is error-handle middleware message

---
+++ seeder.js
@@ -0,0 +1,44 @@
+const fs = require('fs');
+const mongoose = require('mongoose');
+const dotenv = require('dotenv').config({path: './.env'});
+const Bootcamp = require('./src/models/Bootcamp');
+
+// Connect to DB
+mongoose.connect(process.env.MONGO_URI, {
+    useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true, useFindAndModify: false
+});
+
+// Read JSON file
+const bootcamps = JSON.parse(fs.readFileSync(__dirname + '/_data/bootcamps.json'));
+
+// Import bootcamps to DB
+const importData = async () => {
+    try {
+        await Bootcamp.create(bootcamps);
+        console.log('Data Imported...');
+        process.exit();
+    } catch (err) {
+        console.error(err);
+    }
+};
+
+
+// Delete bootcamps from DB
+const deleteData = async () => {
+    try {
+        await Bootcamp.deleteMany();
+        console.log('Data Destroyed...');
+        process.exit();
+    } catch (err) {
+        console.error(err);
+    }
+};
+
+if (process.argv[2] === '-i') {
+    importData();
+} else if(process.argv[2] === '-d') {
+    deleteData();
+}
+
+// To run this seeder we do so from Terminal using CMD -> NODE SEEDERFILENAME -i to import & -d to destroy
+// eg node seeder -i
--- src/middlewares/error-handler.js
+++ src/middlewares/error-handler.js
@@ -15,7 +15,7 @@ const errorHandler = (err, req, res, next) => {
 
     // Mongoose duplicate key
     if (err.code === 11000) {
-        const message = 'Dulicate field value entered';
+        const message = 'Duplicate field value entered';
         error = new ErrorResponse(message, 400);
     }
 
